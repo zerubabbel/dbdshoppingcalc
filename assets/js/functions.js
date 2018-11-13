c6b6c3a3a665d5c4851172482e40d9b311e186f5
zerubabbel <achiappetta90@gmail.com>
2018-11-13 08:46:01 -0800

    trying to fix javascript error

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
 
   // Get started!
-
+  $('#addToListButton').click(function() { 
+		console.log('Hello');
+		$('#inputFieldsArea').append('<strong>Item Price <div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text">$</span></div><input type="text" class="form-control" aria-label="Item Price"><div class="input-group-append"><span class="input-group-text"></span></div></div>');
+	}); 
 });
