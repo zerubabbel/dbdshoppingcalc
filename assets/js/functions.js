b57e46b2f64895a1942960d4c0244dd3f7927a81
zerubabbel <achiappetta90@gmail.com>
2018-11-19 07:32:07 -0800

    completed the footer

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,11 @@
 $( document ).ready(function() {
 
+	var total = 1; 
   // Get started!
   $('#addToListButton').click(function() { 
+  		total++;
 		console.log('Hello');
-		$('#inputFieldsArea').append('<div class="inputBlock"><strong>Item Price</strong><div class="input-group mb-3 mr-2"><div class="input-group-prepend"><span class="input-group-text">$</span></div><input type="text" class="form-control" aria-label="Item Price"><div class="input-group-append mr-2"><span class="input-group-text rounded-right"></span></div><button class="btn-danger rounded remove"><i class="fa fa-trash"></i></button></div></div>');
+		$('#inputFieldsArea').append('<div class="inputBlock number'+total+'"><strong>Item Price</strong><div class="input-group mb-3 mr-2"><div class="input-group-prepend"><span class="input-group-text">$</span></div><input type="text" class="form-control" aria-label="Item Price"><div class="input-group-append mr-2"><span class="input-group-text rounded-right"></span></div><button class="btn-danger rounded remove"><i class="fa fa-trash"></i></button></div></div>');
 	}); 
 
   $('#inputFieldsArea').on('click', '.remove', function() {
